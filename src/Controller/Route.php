9f7a3f91e39aed2d61c361b385b477a93f3ec763
Igor de Paula <principe.borodin@gmail.com>
2015-04-11 22:23:40 -0300

    Add route with params

--- src/Controller/Route.php
+++ src/Controller/Route.php
@@ -6,6 +6,8 @@ class Route
 {
 	private $data = null;
 
+	private $params = array();
+
 	public function __construct($args){
 		$this->data = json_decode(str_replace(array('(',')'),'',$args));
 	}
@@ -14,7 +16,31 @@ class Route
 		return $this->data->method;
 	}
 
+	public function getUri(){
+		$uri = $_SERVER['REQUEST_URI'];
+		preg_match_all("/[a-z0-9]+|:[a-z0-9]*|\[:[a-z0-9]*\]/", $uri , $uriResult);
+		return array('uri'=>$uri,'match'=>count($uriResult[0]),'parts'=>$uriResult[0]);
+	}
+
 	public function getRoute(){
-		return $this->data->name;
+		preg_match_all("/[a-z0-9]+|:[a-z0-9]*|\[:[a-z0-9]*\]/", $this->data->name, $route);
+		$uri = $this->getUri();
+		$countRoute = count($route[0]);
+		if($countRoute == $uri['match']){
+			$params= array_combine($route[0], $uri['parts']);
+			array_walk($params, array($this,'cleanParam'));
+			return true;
+		}
+	}
+
+	public function getParams(){
+		return $this->params;
+	}
+
+	private function cleanParam($value,$key)
+	{
+		if(strpos($key,':')>-1){
+			$this->params[str_replace(':','',$key)] = $value;
+		}
 	}
 }
